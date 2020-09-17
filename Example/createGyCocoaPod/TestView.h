a2496069fc8cd200b62c31930a8fb43b7bfeb3c7
guy <tttt870226@163.com>
2020-09-17 10:41:19 +0800

    first

---
+++ Example/createGyCocoaPod/TestView.h
@@ -0,0 +1,19 @@
+//
+//  TestView.h
+//  XFPodTest_Example
+//
+//  Created by skynj on 2020/9/17.
+//  Copyright © 2020 tttt870226. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestView : UIView
+
+
+@property(nonatomic,retain)NSString* m_test;
+@end
+
+NS_ASSUME_NONNULL_END
